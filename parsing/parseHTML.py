c689bdab04c063ac27a0f3854ec31d6c59165ff6
GDM <jinwook31@naver.com>
2016-07-19 11:43:55 +0900

    LAPAACK

--- parsing/parseHTML.py
+++ parsing/parseHTML.py
@@ -8,11 +8,29 @@ html = conn.read()
 soup = BeautifulSoup(html, 'lxml')
 links = soup.find_all('a')
 
+total = 0
+
 for tag in links:
     link = tag.get('href',None)
     if link is not None:
-	#xconn = urllib2.urlopen(method+link)
-	#html = conn.read()
-	#soup = BeautifulSoup(html, 'lxml')
-	print method+link
-    
+	conn = urllib2.urlopen(method+link)
+	html = conn.read()
+	soup = BeautifulSoup(html, 'html5lib')
+	
+	print link[25:]
+	print total
+
+	count = len(soup.find_all('pre'))
+	length = range(1,count)
+
+	for i in length:
+	    pre = soup.find_all('pre')[i]
+	    res = pre.text.replace("\n", "").replace("  ", "")
+	    if res[0:2] == 'pu' or res[0] == '@':
+	        #print res
+		count += 1
+
+	#print "\n"
+	total += count
+
+print total
