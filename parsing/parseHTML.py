225e8099757a88eb6944f24e3a7c2e9d29d6a0a0
GDM <jinwook31@naver.com>
2016-07-19 15:33:26 +0900

    LAPACK

--- parsing/parseHTML.py
+++ parsing/parseHTML.py
@@ -1,3 +1,5 @@
+#-*-coding: utf-8-*-
+
 import urllib2
 from bs4 import BeautifulSoup
 url = 'http://commons.apache.org/proper/commons-math/javadocs/api-3.6.1/allclasses-frame.html'
@@ -8,6 +10,8 @@ html = conn.read()
 soup = BeautifulSoup(html, 'lxml')
 links = soup.find_all('a')
 
+f = open("/home/gdm1/바탕화면/apache_commons.txt",'w')
+
 total = 0
 
 for tag in links:
@@ -16,21 +20,30 @@ for tag in links:
 	conn = urllib2.urlopen(method+link)
 	html = conn.read()
 	soup = BeautifulSoup(html, 'html5lib')
-	
-	print link[25:]
-	print total
-
+		
 	count = len(soup.find_all('pre'))
 	length = range(1,count)
+	res = ""
 
+	#Get Method to String
 	for i in length:
 	    pre = soup.find_all('pre')[i]
-	    res = pre.text.replace("\n", "").replace("  ", "")
-	    if res[0:2] == 'pu' or res[0] == '@':
-	        #print res
-		count += 1
+	    tmp = pre.text.replace("\n", "").replace("  ", "")
 
-	#print "\n"
-	total += count
+	    if tmp[0:2] == 'pu':
+	        res = tmp
+
+	    if tmp[0:17] == '@Deprecatedpublic':
+		res = res.replace("@Deprecatedpublic","public")
+		tes = tmp
+
+	    if res != "":
+        	write = link[25:len(link)-5]+"\t"+res.replace(",","\t").replace("(","\t")
+		index = write.rfind(')')
+		write = write[0:index]
+		print write
+		f.write(write.replace(u'\xa0', u' ')+"\n")
+		total += 1
 
 print total
+f.close()
